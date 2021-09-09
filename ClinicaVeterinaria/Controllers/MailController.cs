3b85e24ec0227ff5ec23d1fe597ade68c24eae6d
Alcobiaa <goncaloalcobia1904@gmail.com>
2021-09-09 13:50:32 +0100

    Email form

---
+++ ClinicaVeterinaria/Controllers/MailController.cs
@@ -0,0 +1,61 @@
+﻿using ClinicaVeterinaria.Helpers;
+using ClinicaVeterinaria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinaria.Controllers
+{
+    public class MailController : Controller
+    {
+        private readonly IUserHelper _userHelper;
+        private readonly IMailHelper _mailHelper;
+
+        public MailController(IUserHelper userHelper,
+            IMailHelper mailHelper)
+        {
+            _userHelper = userHelper;
+            _mailHelper = mailHelper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(MailViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(model.Email);
+
+                if(user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "The email doesn´t correspont to registered user.");
+                    return View(model);
+                }
+
+                Response response = _mailHelper.SendEmail(model.Email, model.Subject, model.Message);
+
+                if (response.IsSuccess)
+                {
+                    this.ViewBag.Message = "The e-mail has been sent...";
+                }
+
+                return this.View();
+            }
+
+            return this.View(model);
+        }
+    }
+}
---
+++ ClinicaVeterinaria/Models/MailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaVeterinaria.Models
+{
+    public class MailViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
